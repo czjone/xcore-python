ead5a421ca37ce035af1e3bce40e011f00a150c8
solyess <czjone@126.com>
2021-12-01 15:31:23 +0800

    add step.py

---
+++ step.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(name='xcore',
+      version='0.3.1',
+      description='Python extention libs',
+      author='solyess',
+      package_dir={'': 'xcore'},
+      packages=[''],
+     )
